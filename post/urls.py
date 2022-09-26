78c11aaa1b7c4d8a745c9db37c7f7c3619d57170
Joanne <lee.joanne54@gmail.com>
2022-09-26 16:53:15 +0000

    detailed view post

--- post/urls.py
+++ post/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from post import views
 
 urlpatterns = [
-    path('post/', views.PostList.as_view())
+    path('post/', views.PostList.as_view()),
+    path('post/<int:pk>', views.PostDetail.as_view())
 ]
