ae42e5d27bf5b252ba24396573d15321da0556d9
Joanne <lee.joanne54@gmail.com>
2023-08-15 21:02:02 +0000

    Add proxy to package.json

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://dfi-example-project.herokuapp.com/";
+// axios.defaults.baseURL = "https://dfi-example-project.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
