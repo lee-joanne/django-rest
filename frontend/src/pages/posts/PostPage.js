c88670e34c5503d215360e707dffc43af6d42eb8
Joanne <lee.joanne54@gmail.com>
2023-08-15 21:00:05 +0000

    Clone react project and install

---
+++ frontend/src/pages/posts/PostPage.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
+import axios from "axios";
+import Post from "./Post";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Comments from "../comments/Comments";
+import PopularProfiles from "../profiles/PopularProfiles";
+
+function PostPage() {
+    const { id } = useParams();
+    const [post, setPost] = useState({ results: [] });
+
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{ data: post }, { data: comments }] = await Promise.all([
+                    axiosReq.get(`/post/${id}`),
+                    axios.get(`/comments/?post=${id}`),
+                ])
+                setPost({ results: [post] })
+                setComments(comments)
+                console.log(post)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        handleMount()
+    }, [id]);
+
+    return (
+        <Row className="h-100">
+            <Col className="py-2 p-0 p-lg-2" lg={8}>
+                <PopularProfiles mobile />
+                <Post {...post.results[0]} setPosts={setPost} postPage />
+                <Container className={appStyles.Content}>
+                    {currentUser ? (
+                        <CommentCreateForm
+                            profile_id={currentUser.profile_id}
+                            profileImage={profile_image}
+                            post={id}
+                            setPost={setPost}
+                            setComments={setComments}
+                        />
+                    ) : comments.results.length ? (
+                        "Comments"
+                    ) : null}
+                    {comments.results.length ? (
+                        comments.results.map(comment => (
+                            <Comments key={comment.id} {...comment} setPost={setPost} setComment={setComments} />
+                        ))
+                    ) : currentUser ? (
+                        <span>No comments yet, be the first to comment</span>
+                    ) : (
+                        <span>No comments yet</span>
+                    )}
+                </Container>
+            </Col>
+            <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+                <PopularProfiles />
+            </Col>
+        </Row>
+    );
+}
+
+export default PostPage;
