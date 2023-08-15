dbd4b3fe18b4144ab23745ce6aa973dc5ca4e035
Joanne <lee.joanne54@gmail.com>
2023-08-15 22:39:28 +0000

    Final commit before deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -190,7 +190,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASEDIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
 
 # Default primary key field type
